fca3c92654131b5993e065be4e387cae79850294
bdenney <bdenney>
2001-06-13 15:52:04 +0000

    - boot options should say memory options instead!

--- gui/control.cc
+++ gui/control.cc
@@ -252,7 +252,7 @@ static char *startup_options_prompt =
 "1. Log file: %s\n"
 "2. Log options for all devices\n"
 "3. Log options for individual devices\n"
-"4. Boot options\n"
+"4. Memory options\n"
 "5. Interface options\n"
 "6. Disk options\n"
 "7. Sound options\n"
