f7ab6d6685fd964335127a99d61c84cba1116548
fries <fries>
2001-06-19 14:55:34 +0000

    fix compile problem with newer gcc (2.95.3 for me)

--- gui/control.cc
+++ gui/control.cc
@@ -126,7 +126,7 @@ ask_uint (char *prompt, Bit32u min, Bit32u max, Bit32u the_default, Bit32u *out,
       *out = the_default;
       return 0;
     }
-    char *format = (base==10) ? "%d" : "%x";
+    const char *format = (base==10) ? "%d" : "%x";
     illegal = (1 != sscanf (buffer, format, &n));
     if (illegal || n<min || n>max) {
       printf ("Your choice (%s) was not an integer between %u and %u.\n\n",
