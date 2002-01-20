25f9fb688a7ace7d08d76a4787f75fa6bdd3b7be
vruppert <vruppert>
2002-01-20 16:35:32 +0000

    - interrupt handling improved (only loopback works for now) - interrupt reasons modem status change and receiver line status change added - number of data bits is considered now - REMARK: Windows 95 driver still makes trouble - TODO: real serial communication, FIFO, second serial port

--- iodev/serial.h
+++ iodev/serial.h
@@ -2,7 +2,7 @@
 // $Id$
 /////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2001  MandrakeSoft S.A.
+//  Copyright (C) 2002  MandrakeSoft S.A.
 //
 //    MandrakeSoft S.A.
 //    43, rue d'Aboukir
@@ -55,8 +55,14 @@ typedef struct {
    */
   Boolean  rx_empty;
   Boolean  tx_empty;
+  Boolean  ls_interrupt;
+  Boolean  ms_interrupt;
   Boolean  rx_interrupt;
   Boolean  tx_interrupt;
+  Boolean  ls_ipending;
+  Boolean  ms_ipending;
+  Boolean  rx_ipending;
+  Boolean  tx_ipending;
 
   int  baudrate;
   int  tx_timer_index;
