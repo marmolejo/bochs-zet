1452c4026a2443805d3d046d93a3b515e66094b0
instinc <instinc>
2002-04-01 04:02:02 +0000

    check if CTRL+C is pressed while in a HLT instruction

--- cpu/cpu.cc
+++ cpu/cpu.cc
@@ -441,7 +441,7 @@ handle_async_event:
     BX_CPU_THIS_PTR inhibit_mask = 0; // clear inhibits for after resume
     // for one processor, pass the time as quickly as possible until
     // an interrupt wakes up the CPU.
-    while (1) {
+    while (bx_guard.interrupt_requested != 1) {
       if (BX_CPU_THIS_PTR INTR && BX_CPU_THIS_PTR eflags.if_) {
         break;
         }
