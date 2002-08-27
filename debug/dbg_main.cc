422b0e37e060ed43c31d67b40ad48a16639d0938
bdenney <bdenney>
2002-08-27 20:09:00 +0000

    - bx_devices.reset now requires a type arg: BX_RESET_HARDWARE

--- debug/dbg_main.cc
+++ debug/dbg_main.cc
@@ -380,7 +380,7 @@ process_sim2:
 
   // (mch) Moved from main.cc
   bx_devices.init(BX_MEM(0));
-  bx_devices.reset();
+  bx_devices.reset(BX_RESET_HARDWARE);
   SIM->set_init_done (1);
 
   bx_gui.init_signal_handlers ();
