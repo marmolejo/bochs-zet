d058f9df59c7f4b1c59b592b06347dd3e1c631f6
kevinlawton <kevinlawton>
2002-09-30 16:43:59 +0000

    As a 1st effort to understand/debug the timer code, I cleanup   up pc_system.h.  Moved all variables under the private: section,   as well as a few member functions.  The string instructions   were accessing a field directly (only reads), so I indirected   that via an inline member function for better abstraction.

--- cpu/string.cc
+++ cpu/string.cc
@@ -212,8 +212,8 @@ BX_CPU_C::MOVSB_XbYb(bxInstruction_c *i)
             byteCount = bytesFitSrc;
           if (byteCount > bytesFitDst)
             byteCount = bytesFitDst;
-          if (byteCount > bx_pc_system.num_cpu_ticks_left)
-            byteCount = bx_pc_system.num_cpu_ticks_left;
+          if (byteCount > bx_pc_system.getNumCpuTicksLeftNextEvent())
+            byteCount = bx_pc_system.getNumCpuTicksLeftNextEvent();
 
           // If after all the restrictions, there is anything left to do...
           if (byteCount) {
@@ -528,8 +528,8 @@ BX_CPU_C::MOVSW_XvYv(bxInstruction_c *i)
             dwordCount = dwordsFitSrc;
           if (dwordCount > dwordsFitDst)
             dwordCount = dwordsFitDst;
-          if (dwordCount > bx_pc_system.num_cpu_ticks_left)
-            dwordCount = bx_pc_system.num_cpu_ticks_left;
+          if (dwordCount > bx_pc_system.getNumCpuTicksLeftNextEvent())
+            dwordCount = bx_pc_system.getNumCpuTicksLeftNextEvent();
 
           // If after all the restrictions, there is anything left to do...
           if (dwordCount) {
@@ -786,8 +786,8 @@ doIncr32:
             wordCount = wordsFitSrc;
           if (wordCount > wordsFitDst)
             wordCount = wordsFitDst;
-          if (wordCount > bx_pc_system.num_cpu_ticks_left)
-            wordCount = bx_pc_system.num_cpu_ticks_left;
+          if (wordCount > bx_pc_system.getNumCpuTicksLeftNextEvent())
+            wordCount = bx_pc_system.getNumCpuTicksLeftNextEvent();
 
           // If after all the restrictions, there is anything left to do...
           if (wordCount) {
@@ -1648,8 +1648,8 @@ BX_CPU_C::STOSB_YbAL(bxInstruction_c *i)
           // source or dest pages.
           if (byteCount > bytesFitDst)
             byteCount = bytesFitDst;
-          if (byteCount > bx_pc_system.num_cpu_ticks_left)
-            byteCount = bx_pc_system.num_cpu_ticks_left;
+          if (byteCount > bx_pc_system.getNumCpuTicksLeftNextEvent())
+            byteCount = bx_pc_system.getNumCpuTicksLeftNextEvent();
 
           // If after all the restrictions, there is anything left to do...
           if (byteCount) {
