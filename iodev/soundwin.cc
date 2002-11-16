dd78944f73a72b29a345e44d2f9569af45fe872d
vruppert <vruppert>
2002-11-16 10:29:56 +0000

    - the SB16 plugin dll needs the link option -lwinmm - BX_PLUGGABLE must be defined in soundwin.cc

--- iodev/soundwin.cc
+++ iodev/soundwin.cc
@@ -28,6 +28,11 @@
 
 #if defined(WIN32)
 
+// Define BX_PLUGGABLE in files that can be compiled into plugins.  For
+// platforms that require a special tag on exported symbols, BX_PLUGGABLE 
+// is used to know when we are exporting symbols and when we are importing.
+#define BX_PLUGGABLE
+
 #include "bochs.h"
 #define LOG_THIS bx_devices.pluginSB16Device->
 
