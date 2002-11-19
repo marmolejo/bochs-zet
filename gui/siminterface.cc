c1d0a106f042ee1ccc10abb3e33952750822ce6a
bdenney <bdenney>
2002-11-19 05:54:25 +0000

    - VC++ didn't like the fact that we were deleting a const char*.  So I   typecast it to a char* and THEN deleted it!

--- gui/siminterface.cc
+++ gui/siminterface.cc
@@ -660,7 +660,7 @@ void bx_real_sim_c::debug_puts (const char *text)
 #else
   // text mode debugger: just write to console
   fputs (text, stderr);
-  delete [] text;
+  delete [] (char *)text;
 #endif
 }
 #endif
