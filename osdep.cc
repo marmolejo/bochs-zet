a5d0356cca790f8a682fb920c15eb4d1c193efc9
bdenney <bdenney>
2002-12-05 17:38:00 +0000

    - MSVC++ does not allow the "LL" suffix.  Fortunately I already have a   configure test and macro BX_CONST64() to handle this problem.  Changed   numbers like 1000LL into BX_CONST64(1000).

--- osdep.cc
+++ osdep.cc
@@ -294,15 +294,15 @@ Bit64u bx_get_realtime64_usec (void) {
 Bit64u last_realtime64_top = 0;
 Bit64u last_realtime64_bottom = 0;
 Bit64u bx_get_realtime64_usec (void) {
-  Bit64u new_bottom = ((Bit64u) GetTickCount()) & 0x0FFFFFFFFll;
+  Bit64u new_bottom = ((Bit64u) GetTickCount()) & BX_CONST64(0x0FFFFFFFF);
   if(new_bottom < last_realtime64_bottom) {
-    last_realtime64_top += 0x0000000100000000ll;
+    last_realtime64_top += BX_CONST64(0x0000000100000000);
   }
   last_realtime64_bottom = new_bottom;
   Bit64u interim_realtime64 =
-    (last_realtime64_top & 0xFFFFFFFF00000000ll) |
-    (new_bottom          & 0x00000000FFFFFFFFll);
-  return interim_realtime64*((Bit64u)1000ll);
+    (last_realtime64_top & BX_CONST64(0xFFFFFFFF00000000)) |
+    (new_bottom          & BX_CONST64(0x00000000FFFFFFFF));
+  return interim_realtime64*(BX_CONST64(1000));
 }
 #    endif
 #  endif
