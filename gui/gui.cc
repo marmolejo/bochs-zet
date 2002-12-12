8a1b7123c079f6da78e4a9da8c466b0cedd9663f
yakovlev <yakovlev>
2002-12-12 03:41:51 +0000

    make_text_snapshot returns a length value.
    We now use that instead of calling strlen.

--- gui/gui.cc
+++ gui/gui.cc
@@ -345,7 +345,7 @@ bx_gui_c::copy_handler(void)
   if (!BX_GUI_THIS set_clipboard_text(text_snapshot, len)) {
     // platform specific code failed, use portable code instead
     FILE *fp = fopen("copy.txt", "w");
-    fwrite(text_snapshot, 1, strlen(text_snapshot), fp);
+    fwrite(text_snapshot, 1, len, fp);
     fclose(fp);
   }
   free(text_snapshot);
@@ -376,7 +376,7 @@ bx_gui_c::snapshot_handler(void)
     strcpy (filename, "snapshot.txt");
   }
   FILE *fp = fopen(filename, "wb");
-  fwrite(text_snapshot, 1, strlen(text_snapshot), fp);
+  fwrite(text_snapshot, 1, len, fp);
   fclose(fp);
   free(text_snapshot);
 }
