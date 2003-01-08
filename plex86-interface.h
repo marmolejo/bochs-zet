bff40d5cc83c636026d5d6b35a277fea4df8cb22
kevinlawton <kevinlawton>
2003-01-08 17:22:06 +0000

    Fixes these files to comply with the new method of getting   and pinning user pages from the plex86 kernel module.  The   guest physical memory array is no longer mmap()'d from kernel   space where it used to be allocated.

--- plex86-interface.h
+++ plex86-interface.h
@@ -29,12 +29,12 @@
 
 extern unsigned     plex86State;
 extern int          plex86FD;
-extern Bit8u       *plex86MemPtr;
 extern unsigned     plex86MemSize;
 extern Bit8u       *plex86PrintBuffer;
 extern guest_cpu_t *plex86GuestCPU;
 
 extern unsigned plex86CpuInfo(BX_CPU_C *cpu);
-extern unsigned char * plex86AllocateMemory(unsigned nMegs);
 extern unsigned plex86TearDown(void);
 extern unsigned plex86ExecuteInVM(BX_CPU_C *cpu);
+extern unsigned plex86RegisterGuestMemory(Bit8u *vector, unsigned bytes);
+extern unsigned plex86UnregisterGuestMemory(Bit8u *vector, unsigned bytes);
