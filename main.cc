a8ad7294fe967d80fa976c7eb25beaab342db804
vruppert <vruppert>
2003-09-15 17:05:33 +0000

    - initial value of parameter 'time0' set to 1 (localtime)

--- main.cc
+++ main.cc
@@ -1419,7 +1419,7 @@ void bx_init_options ()
       "clock:time0",
       "Initial time for Bochs CMOS clock, used if you really want two runs to be identical",
       0, BX_MAX_BIT32U,
-      0);
+      BX_CLOCK_TIME0_LOCAL);
   bx_options.clock.Osync = new bx_param_enum_c (BXP_CLOCK_SYNC,
       "clock:sync",
       "Host to guest time synchronization method",
