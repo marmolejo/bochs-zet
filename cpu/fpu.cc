4de6bb8447f38d98eb853383e838193da114e0f9
sshwarts <sshwarts>
2004-01-31 15:11:41 +0000

    fix bug

--- cpu/fpu.cc
+++ cpu/fpu.cc
@@ -41,15 +41,18 @@ void BX_CPU_C::prepareFPU(void)
 
 void BX_CPU_C::FPU_check_pending_exceptions(void)
 {
-  if (BX_CPU_THIS_PTR cr0.ne == 0)
+  if(FPU_PARTIAL_STATUS & FPU_SW_SUMMARY)
   {
-    // MSDOS compatibility external interrupt (IRQ13)
-    BX_INFO (("math_abort: MSDOS compatibility FPU exception"));
-    DEV_pic_raise_irq(13);
-  }
-  else
-  {
-    exception(BX_MF_EXCEPTION, 0, 0);
+    if (BX_CPU_THIS_PTR cr0.ne == 0)
+    {
+      // MSDOS compatibility external interrupt (IRQ13)
+      BX_INFO (("math_abort: MSDOS compatibility FPU exception"));
+      DEV_pic_raise_irq(13);
+    }
+    else
+    {
+      exception(BX_MF_EXCEPTION, 0, 0);
+    }
   }
 }
 #endif
