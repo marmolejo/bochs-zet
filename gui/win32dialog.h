9eb777281e709b1e7661c00170296f3c5b0cc727
vruppert <vruppert>
2004-02-01 01:40:14 +0000

    - new gui dialogs for the first cdrom and the runtime options (log options are   still handled on the console) - minor changes in other win32dialog functions

--- gui/win32dialog.h
+++ gui/win32dialog.h
@@ -12,8 +12,10 @@ extern "C" {
 
 HWND GetBochsWindow();
 void LogAskDialog(BxEvent *event);
-int AskFilename(HWND hwnd, bx_param_filename_c *param);
+int AskFilename(HWND hwnd, bx_param_filename_c *param, const char *ext);
 int AskString(bx_param_string_c *param);
 int FloppyDialog(bx_param_filename_c *param);
+int Cdrom1Dialog();
+int RuntimeOptionsDialog();
 
 #endif
