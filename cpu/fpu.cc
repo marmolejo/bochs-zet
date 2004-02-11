cfcb5bc313cda5429e0517e291ffe1a73c09da2d
sshwarts <sshwarts>
2004-02-11 20:04:34 +0000

    remove duplicated include

--- cpu/fpu.cc
+++ cpu/fpu.cc
@@ -24,8 +24,6 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
 
-#include "bochs.h"
-
 #define NEED_CPU_REG_SHORTCUTS 1
 #include "bochs.h"
 #define LOG_THIS BX_CPU_THIS_PTR
