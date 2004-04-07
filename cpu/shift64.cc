9748e99d48e5a835b55c8ad2adc555d47ae34c26
sshwarts <sshwarts>
2004-04-07 19:22:59 +0000

    little bit clean 64b code

--- cpu/shift64.cc
+++ cpu/shift64.cc
@@ -81,7 +81,6 @@ BX_CPU_C::SHLD_EqGq(bxInstruction_c *i)
     set_SF(result_64 >> 63);
 }
 
-
   void
 BX_CPU_C::SHRD_EqGq(bxInstruction_c *i)
 {
@@ -94,7 +93,6 @@ BX_CPU_C::SHRD_EqGq(bxInstruction_c *i)
     count = CL & 0x3f;
 
   if (!count) return; /* NOP */
-
 
     /* op1 is a register or memory reference */
     if (i->modC0()) {
@@ -228,7 +226,6 @@ BX_CPU_C::RCL_Eq(bxInstruction_c *i)
     count = 1;
   else // (i->b1() == 0xd3)
     count = CL & 0x3f;
-
 
     /* op1 is a register or memory reference */
     if (i->modC0()) {
