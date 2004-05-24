aa8b723948f331b061055f57aac549e46eb28170
sshwarts <sshwarts>
2004-05-24 16:49:45 +0000

    elliminated reg_u_add from old FPU

--- fpu/fpu_emu.h
+++ fpu/fpu_emu.h
@@ -95,9 +95,6 @@ asmlinkage int FPU_normalize_nuo(FPU_REG *x, int bias);
 asmlinkage int FPU_u_mul(FPU_REG const *arg1, FPU_REG const *arg2,
 			 FPU_REG *answ, u16 control_w, u_char sign,
 			 s32 expon);
-asmlinkage int FPU_u_add(FPU_REG const *arg1, FPU_REG const *arg2,
-			 FPU_REG *answ, u16 control_w, u_char sign,
-			 s32 expa, s32 expb);
 asmlinkage u32 FPU_shrx(void *l, u32 x);
 asmlinkage u32 FPU_shrxs(void *v, u32 x);
 asmlinkage int FPU_round(FPU_REG *arg, u32 extent,
