a28967efa7d50cd072c5a4b379a5552470f276bb
vruppert <vruppert>
2004-08-16 15:23:19 +0000

    - extended bitblt mode solidfill implemented - svga_patterncopy() fixed - svga_pitch change fixed - forward write to MMIO address 0x13 to graphics controller register 0x2b

--- iodev/svga_cirrus.h
+++ iodev/svga_cirrus.h
@@ -150,6 +150,7 @@ private:
 #endif // !BX_USE_CIRRUS_SMF
   BX_CIRRUS_SMF void svga_patterncopy();
   BX_CIRRUS_SMF void svga_simplebitblt();
+  BX_CIRRUS_SMF void svga_solidfill();
   BX_CIRRUS_SMF void svga_patterncopy_memsrc();
   BX_CIRRUS_SMF void svga_simplebitblt_memsrc();
   BX_CIRRUS_SMF void svga_simplebitblt_transp_memsrc();
@@ -211,6 +212,7 @@ private:
     Bit8u bltrop;
     Bit8u *dst;
     const Bit8u *src;
+    Bit32u srcaddr;
 #if BX_USE_CIRRUS_SMF
     void (*bitblt_ptr)();
 #else
