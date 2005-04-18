bdc10e166d88317a624f6c5bc54b98bdfdbde13a
sshwarts <sshwarts>
2005-04-18 17:20:27 +0000

    Fix compilation errors Add BX_INFO for writing to TSC_MSR (not implemented message)

--- instrument/stubs/instrument.h
+++ instrument/stubs/instrument.h
@@ -94,9 +94,9 @@ void bx_instr_tlb_cntrl(unsigned cpu, unsigned what, Bit32u newval);
 void bx_instr_cache_cntrl(unsigned cpu, unsigned what);
 void bx_instr_prefetch_hint(unsigned cpu, unsigned what, unsigned seg, bx_address offset);
 
-void bx_instr_before_execution(unsigned cpu);
-void bx_instr_after_execution(unsigned cpu);
-void bx_instr_repeat_iteration(unsigned cpu);
+void bx_instr_before_execution(unsigned cpu, const bxInstruction_c *i);
+void bx_instr_after_execution(unsigned cpu, const bxInstruction_c *i);
+void bx_instr_repeat_iteration(unsigned cpu, const bxInstruction_c *i);
 
 void bx_instr_inp(Bit16u addr, unsigned len);
 void bx_instr_outp(Bit16u addr, unsigned len);
