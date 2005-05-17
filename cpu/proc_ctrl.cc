4b01053580a3b515324a2b71c00186d53f3ff0bb
kevinlawton <kevinlawton>
2005-05-17 22:22:35 +0000

    Typo in CR8 handling in MOV_CqRq/MOV_RqCq had a typo.  A switch   target of 7 was used instead of 8.

--- cpu/proc_ctrl.cc
+++ cpu/proc_ctrl.cc
@@ -843,7 +843,7 @@ void BX_CPU_C::MOV_CqRq(bxInstruction_c *i)
       SetCR4(val_64);
       break;
 #if BX_SUPPORT_APIC
-    case 7: // CR8
+    case 8: // CR8
       // CR8 is aliased to APIC->TASK PRIORITY register
       //   APIC.TPR[7:4] = CR8[3:0]
       //   APIC.TPR[3:0] = 0
@@ -915,7 +915,7 @@ void BX_CPU_C::MOV_RqCq(bxInstruction_c *i)
       val_64 = BX_CPU_THIS_PTR cr4.getRegister();
       break;
 #if BX_SUPPORT_APIC
-    case 7: // CR8
+    case 8: // CR8
       // CR8 is aliased to APIC->TASK PRIORITY register
       //   APIC.TPR[7:4] = CR8[3:0]
       //   APIC.TPR[3:0] = 0
@@ -2280,3 +2280,7 @@ Bit32u BX_CPU_C::hwdebug_compare(Bit32u laddr_0, unsigned size,
 }
 
 #endif
+
+void BX_CPU_C::LFENCE(bxInstruction_c *i) {}
+void BX_CPU_C::MFENCE(bxInstruction_c *i) {}
+void BX_CPU_C::SFENCE(bxInstruction_c *i) {}
