f80a19b49c4f0537910bcf29609940349b6b6e16
vruppert <vruppert>
2005-07-27 19:17:29 +0000

    - added simple speaker support for OS X using the default system beep   (patch by brianonn@telus.net)

--- iodev/speaker.cc
+++ iodev/speaker.cc
@@ -34,6 +34,11 @@
 #include <linux/kd.h>
 #endif
 
+#if defined(__APPLE__) || defined(macintosh)
+#define Float32 KLUDGE_Float32  // Carbon headers define Float32 and Float64 
+#define Float64 KLUDGE_Float64  // and conflict with bochs definitions
+#include <Carbon/Carbon.h>      // but this code doesn't need them.
+#endif
 
 #define LOG_THIS theSpeaker->
 
@@ -105,6 +110,8 @@ void bx_speaker_c::beep_on(float frequency)
   }
 #elif defined(WIN32)
   usec_start = bx_pc_system.time_usec();
+#elif defined(__APPLE__) || defined(macintosh)
+  AlertSoundPlay();
 #endif
    
   // give the gui a chance to signal beep off
