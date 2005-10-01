21944bc84537c823b105258c580af9c7ec73015d
sshwarts <sshwarts>
2005-10-01 07:47:00 +0000

    LTR should #GP when loading NULL selector fixed check for SYSENTER/SYSEXIT instructions according to new Intel references

--- cpu/bcd.cc
+++ cpu/bcd.cc
@@ -122,9 +122,9 @@ void BX_CPU_C::AAM(bxInstruction_c *i)
 
   /* The following behaviour seems to match the P6 and 
      its derived processors. */
-  set_OF(0);	/* undocumented flag modification */
-  set_AF(0);
-  set_CF(0);
+  clear_OF();	/* undocumented flag modification */
+  clear_AF();
+  clear_CF();
 
   /* AAM affects the following flags: S,Z,P */
   set_SF(AL >= 0x80);
@@ -148,9 +148,9 @@ void BX_CPU_C::AAD(bxInstruction_c *i)
 
   /* The following behaviour seems to match the P6 and 
      its derived processors. */
-  set_OF(0);	/* undocumented flag modification */
-  set_AF(0);
-  set_CF(0);
+  clear_OF();	/* undocumented flag modification */
+  clear_AF();
+  clear_CF();
 
   set_SF(AL >= 0x80);
   set_ZF(AL == 0);
--- cpu/proc_ctrl.cc
+++ cpu/proc_ctrl.cc
@@ -1822,7 +1822,7 @@ void BX_CPU_C::SYSENTER (bxInstruction_c *i)
     exception (BX_GP_EXCEPTION, 0, 0);
     return;
   }
-  if (BX_CPU_THIS_PTR sysenter_cs_msr == 0) {
+  if (BX_SELECTOR_RPL_MASK(BX_CPU_THIS_PTR sysenter_cs_msr) == 0) {
     BX_INFO (("sysenter with zero sysenter_cs_msr"));
     exception (BX_GP_EXCEPTION, 0, 0);
     return;
@@ -1882,7 +1882,7 @@ void BX_CPU_C::SYSEXIT (bxInstruction_c *i)
     exception (BX_GP_EXCEPTION, 0, 0);
     return;
   }
-  if (BX_CPU_THIS_PTR sysenter_cs_msr == 0) {
+  if (BX_SELECTOR_RPL_MASK(BX_CPU_THIS_PTR sysenter_cs_msr) == 0) {
     BX_INFO (("sysexit with zero sysenter_cs_msr"));
     exception (BX_GP_EXCEPTION, 0, 0);
     return;
