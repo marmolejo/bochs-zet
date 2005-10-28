f3c36be8ac5e0a31b7b48c793ed1efcb585b15c0
vruppert <vruppert>
2005-10-28 13:49:24 +0000

    - fixed compile error in case the unimplemented compressed hd support is enabled - using unimplemented hd mode causes error "not implemented yet"

--- gui/siminterface.h
+++ gui/siminterface.h
@@ -1283,9 +1283,9 @@ enum {
 #define BX_ATA_MODE_UNDOABLE    6
 #define BX_ATA_MODE_GROWING     7
 #define BX_ATA_MODE_VOLATILE    8
-#define BX_ATA_MODE_LAST        8
-//#define BX_ATA_MODE_Z_UNDOABLE  9
-//#define BX_ATA_MODE_Z_VOLATILE  10
+#define BX_ATA_MODE_Z_UNDOABLE  9
+#define BX_ATA_MODE_Z_VOLATILE  10
+#define BX_ATA_MODE_LAST        10
 
 #define BX_CLOCK_SYNC_NONE     0
 #define BX_CLOCK_SYNC_REALTIME 1
