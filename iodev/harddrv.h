cd2ac06bb389f66f195a897dc5d11c95f8b09051
vruppert <vruppert>
2005-11-06 08:21:38 +0000

    - ATAPI command "read subchannel format 2" now returns "UPC not recorded" - changed bool types to bx_bool, false and true to 0 and 1 - some indent mode fixes

--- iodev/harddrv.h
+++ iodev/harddrv.h
@@ -722,7 +722,7 @@ private:
   BX_HD_SMF void identify_ATAPI_drive(Bit8u channel);
   BX_HD_SMF void command_aborted(Bit8u channel, unsigned command);
 
-  BX_HD_SMF void init_send_atapi_command(Bit8u channel, Bit8u command, int req_length, int alloc_length, bool lazy = false) BX_CPP_AttrRegparmN(3);
+  BX_HD_SMF void init_send_atapi_command(Bit8u channel, Bit8u command, int req_length, int alloc_length, bx_bool lazy = false) BX_CPP_AttrRegparmN(3);
   BX_HD_SMF void ready_to_send_atapi(Bit8u channel) BX_CPP_AttrRegparmN(1);
   BX_HD_SMF void raise_interrupt(Bit8u channel) BX_CPP_AttrRegparmN(1);
   BX_HD_SMF void atapi_cmd_error(Bit8u channel, sense_t sense_key, asc_t asc, bx_bool show);
