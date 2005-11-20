3b14534cc18eef50c2ee845943c909eee9f78cbb
vruppert <vruppert>
2005-11-20 17:22:42 +0000

    - floppy image size autodetection now available in the whole config interface   (it was only present in bochsrc parser) - floppy media type is now a runtime parameter (TODO: full media type support   needs some work in the floppy emulation code) - floppy media type added to win32 floppy dialog

--- gui/siminterface.h
+++ gui/siminterface.h
@@ -1262,6 +1262,7 @@ enum {
 #define BX_FLOPPY_320K   18 // 320K  5.25"
 #define BX_FLOPPY_LAST   18 // last legal value of floppy type
 
+#define BX_FLOPPY_AUTO     19 // autodetect image size
 #define BX_FLOPPY_UNKNOWN  20 // image size doesn't match one of the types above
 
 #define BX_ATA_DEVICE_DISK      0
