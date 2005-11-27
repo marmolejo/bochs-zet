69819b380fa034b34e1440c09460aafad6c25f9a
vruppert <vruppert>
2005-11-27 14:15:11 +0000

    - gcc warning fixed

--- gui/win32dialog.cc
+++ gui/win32dialog.cc
@@ -191,7 +191,7 @@ static BOOL CALLBACK FloppyDlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lP
           if (AskFilename(hDlg, param, "img") > 0) {
             SetWindowText(GetDlgItem(hDlg, IDPATH), param->getptr());
             SendMessage(GetDlgItem(hDlg, IDSTATUS), BM_SETCHECK, BST_CHECKED, 0);
-            SendMessage(GetDlgItem(hDlg, IDMEDIATYPE), CB_SELECTSTRING, -1, (LPARAM)"auto");
+            SendMessage(GetDlgItem(hDlg, IDMEDIATYPE), CB_SELECTSTRING, (WPARAM)-1, (LPARAM)"auto");
             EnableWindow(GetDlgItem(hDlg, IDCREATE), FALSE);
           }
           return TRUE;
