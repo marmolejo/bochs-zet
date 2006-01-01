8ff4c88149872e45d830a5d0e1d8b1f8934adae0
vruppert <vruppert>
2006-01-01 11:33:06 +0000

    - I/O APIC signal handling rewritten ("backported" from qemu) - don't flood the logfile if APIC EOI has no effect - fixed a warning in the APIC code - TODO: fix IRQ 0 handling, implement ExtINT

--- iodev/pic.cc
+++ iodev/pic.cc
@@ -618,8 +618,8 @@ bx_pic_c::lower_irq(unsigned irq_no)
 {
 #if BX_SUPPORT_APIC
   // forward this function call to the ioapic too
-  if (DEV_ioapic_present()) {
-    bx_devices.ioapic->lower_irq(irq_no, BX_IOAPIC_INT_FROM_ISA);
+  if (DEV_ioapic_present() && (irq_no != 0) && (irq_no != 2)) {
+    bx_devices.ioapic->set_irq_level(irq_no, 0);
   }
 #endif
 
@@ -648,8 +648,8 @@ bx_pic_c::raise_irq(unsigned irq_no)
 {
 #if BX_SUPPORT_APIC
   // forward this function call to the ioapic too
-  if (DEV_ioapic_present()) {
-    bx_devices.ioapic->raise_irq(irq_no, BX_IOAPIC_INT_FROM_ISA);
+  if (DEV_ioapic_present() && (irq_no != 0) && (irq_no != 2)) {
+    bx_devices.ioapic->set_irq_level(irq_no, 1);
   }
 #endif
 
