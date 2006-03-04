b641dee694e0b16b9e0f66c0480fb91d49232e3d
sshwarts <sshwarts>
2006-03-04 09:24:31 +0000

    Missed }

--- cpu/cpu.cc
+++ cpu/cpu.cc
@@ -227,6 +227,7 @@ void BX_CPU_C::cpu_loop(Bit32s max_instr_count)
   if (BX_CPU_THIS_PTR trace) {
     // print the instruction that is about to be executed.
     bx_dbg_disassemble_current(BX_CPU_ID, 1);  // only one cpu, print time stamp
+  }
 #endif
 
   // decoding instruction compeleted -> continue with execution
