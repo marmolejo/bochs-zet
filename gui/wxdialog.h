97f94a6855ad7d1da88efe9bc72cc6edc1d50f19
vruppert <vruppert>
2006-03-26 15:52:31 +0000

    - header of log options table is now always visible - removed unnecessary spaces after function names

--- gui/wxdialog.h
+++ gui/wxdialog.h
@@ -246,7 +246,7 @@ private:
 "from a particular source.  For example if you are having problems with\n"    \
 "the keyboard, you could ask for debug and info events from the keyboard\n"   \
 "to be reported.")
-#define ADVLOG_OPTS_TYPE_NAMES { wxT("debug"), wxT("Info"), wxT("Error"), wxT("Panic"), wxT("Pass") }
+#define ADVLOG_OPTS_TYPE_NAMES { wxT("Debug"), wxT("Info"), wxT("Error"), wxT("Panic"), wxT("Pass") }
 #define ADVLOG_OPTS_N_TYPES 5
 #define ADVLOG_DEFAULTS wxT("Use defaults for all devices")
   void Init();  // called automatically by ShowModal()
@@ -254,7 +254,7 @@ private:
   wxBoxSizer *vertSizer, *logfileSizer, *buttonSizer;
   wxScrolledWindow *scrollWin;
   wxPanel *scrollPanel;
-  wxGridSizer *gridSizer;
+  wxGridSizer *headerSizer, *gridSizer;
   wxTextCtrl *logfile;
   wxButton *applyDefault;
   // 2d array of wxChoice pointers. Each wxChoice* is action[dev][type].
