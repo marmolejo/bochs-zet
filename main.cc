8919ac4cb5a6e4c52218a6a546e29f40b0154be6
vruppert <vruppert>
2006-04-09 09:05:29 +0000

    - save/restore of log options implemented (using bochsrc style) - log options must be restored after initializing all devices - menu items and descriptions for save/restore updated - TODO: implement access to save/restore functions for win32 and wx

--- main.cc
+++ main.cc
@@ -467,7 +467,7 @@ void print_usage()
     "  -f configfile    specify configuration file\n"
     "  -q               quick start (skip configuration interface)\n"
 #if BX_SAVE_RESTORE
-    "  -r               restore Bochs session\n"
+    "  -r path          restore Bochs config and log options from path\n"
 #endif
     "  --help           display this help and exit\n\n"
     "For information on Bochs configuration file arguments, see the\n"
@@ -896,11 +896,6 @@ int bx_init_hardware()
       io->set_log_action (level, action);
     }
   }
-#if BX_SAVE_RESTORE
-  if (SIM->get_param_bool(BXPN_RESTORE_FLAG)->get()) {
-    SIM->restore_logopts();
-  }
-#endif
 
   bx_pc_system.initialize(SIM->get_param_num(BXPN_IPS)->get());
 
@@ -1007,6 +1002,11 @@ int bx_init_hardware()
 #endif
 
   DEV_init_devices();
+#if BX_SAVE_RESTORE
+  if (SIM->get_param_bool(BXPN_RESTORE_FLAG)->get()) {
+    SIM->restore_logopts();
+  }
+#endif
   // will enable A20 line and reset CPU and devices
   bx_pc_system.Reset(BX_RESET_HARDWARE);
 #if BX_SAVE_RESTORE
