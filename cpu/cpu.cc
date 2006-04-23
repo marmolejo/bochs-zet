f756bdce245af70e8588d0887fa21924bd5eb5f6
sshwarts <sshwarts>
2006-04-23 15:37:25 +0000

    Fixed: With cvs snapshot 2006-04-09, --enable-debugger does not show the executed instructions anymore after being enabled via iodebug or "trace on".

--- cpu/cpu.cc
+++ cpu/cpu.cc
@@ -231,7 +231,11 @@ void BX_CPU_C::cpu_loop(Bit32s max_instr_count)
 #if BX_DISASM
   if (BX_CPU_THIS_PTR trace) {
     // print the instruction that is about to be executed
+#if BX_DEBUGGER
+    bx_dbg_disassemble_current(BX_CPU_ID, 1);  // only one cpu, print time stamp
+#else
     debug_disasm_instruction(BX_CPU_THIS_PTR prev_eip);
+#endif
   }
 #endif
 
