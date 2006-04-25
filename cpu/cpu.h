f6e83f92e688926a2659ec53f13035ad4c96a902
sshwarts <sshwarts>
2006-04-25 14:42:56 +0000

    Added assertion check CPU method, could be used for "debug mode" run with checking various assumptions before each instruction emulation

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -2869,6 +2869,7 @@ public: // for now...
   BX_SMF bx_bool can_pop(Bit32u bytes);
   BX_SMF void    decrementESPForPush(unsigned nBytes, Bit32u *eSP);
   BX_SMF void    sanity_checks(void);
+  BX_SMF void    assert_checks(void);
   BX_SMF void    enter_system_management_mode(void);
   BX_SMF void    deliver_NMI(void);
   BX_SMF void    deliver_SMI(void);
