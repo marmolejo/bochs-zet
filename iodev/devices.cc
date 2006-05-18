9f05d105eaa35fcb767cbb29912134a17f261118
sshwarts <sshwarts>
2006-05-18 19:57:11 +0000

    Wow, I forgot to commit this change for NullTimer fixes !

--- iodev/devices.cc
+++ iodev/devices.cc
@@ -140,7 +140,11 @@ void bx_devices_c::init(BX_MEM_C *newmem)
 
   for (i=0; i < BX_MAX_IRQS; i++) {
     irq_handler_name[i] = NULL;
-    }
+  }
+
+  // register as soon as possible - the devices want to have their timers !
+  bx_virt_timer.init();
+  bx_slowdown_timer.init();
 
   // BBD: At present, the only difference between "core" and "optional"
   // plugins is that initialization and reset of optional plugins is handled
@@ -258,10 +262,6 @@ void bx_devices_c::init(BX_MEM_C *newmem)
   /*--- 8254 PIT ---*/
   pit = & bx_pit;
   pit->init();
-
-  bx_virt_timer.init();
-
-  bx_slowdown_timer.init();
 
 #if BX_SUPPORT_IODEBUG
   iodebug = &bx_iodebug;
