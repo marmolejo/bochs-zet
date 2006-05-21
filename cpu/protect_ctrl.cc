49bed7b0f928e9cd37dda4b5e9744f07d14106f3
sshwarts <sshwarts>
2006-05-21 20:41:48 +0000

    Merge tss286 and tss386 segment descriptor cache fields to one structure

--- cpu/protect_ctrl.cc
+++ cpu/protect_ctrl.cc
@@ -439,10 +439,10 @@ void BX_CPU_C::LTR_Ew(bxInstruction_c *i)
   if (BX_CPU_THIS_PTR cpu_mode == BX_MODE_LONG_64) {
     // set upper 32 bits of tss base
     access_linear(BX_CPU_THIS_PTR gdtr.base + selector.index*8 + 8, 4, 0, BX_READ, &dword3);
-    descriptor.u.tss386.base |= ((Bit64u)dword3 << 32);
+    descriptor.u.tss.base |= ((Bit64u)dword3 << 32);
     BX_INFO(("64 bit tss base = 0x%08x%08x",
-       (Bit32u)(descriptor.u.tss386.base >> 32),
-       (Bit32u) descriptor.u.tss386.base));
+       (Bit32u)(descriptor.u.tss.base >> 32),
+       (Bit32u) descriptor.u.tss.base));
   }
 #endif
 
