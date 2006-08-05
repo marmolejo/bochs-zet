4f5347f8386f86065ec2aa3c40df10511697b063
vruppert <vruppert>
2006-08-05 14:42:47 +0000

    - fixed compile error on FreeBSD 5.4 (tested with SF compile farm)

--- iodev/eth_vde.cc
+++ iodev/eth_vde.cc
@@ -41,13 +41,17 @@
 #include <sys/poll.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <asm/types.h>
 #include <sys/socket.h>
+#if defined(__linux__)
+#include <asm/types.h>
+#include <linux/netlink.h>
+#include <linux/if.h>
+#elif BX_HAVE_NET_IF_H
+#include <net/if.h>
+#endif
 #include <sys/uio.h>
 #include <sys/wait.h>
 #include <sys/un.h>
-#include <linux/netlink.h>
-#include <linux/if.h>
 #include <assert.h>
 #include <fcntl.h>
 #include <errno.h>
