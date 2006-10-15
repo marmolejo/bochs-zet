db6cf6c0910888d7bca54920b0a439c6a87e67ca
vruppert <vruppert>
2006-10-15 16:23:09 +0000

    - enable the left "meta" key (left "win" key on win32)

--- gui/sdl.cc
+++ gui/sdl.cc
@@ -916,7 +916,7 @@ static Bit32u sdl_sym_to_bx_key (SDLKey sym)
     case SDLK_RALT:                 return BX_KEY_ALT_R;
     case SDLK_LALT:                 return BX_KEY_ALT_L;
     case SDLK_RMETA:                return BX_KEY_ALT_R;
-//  case SDLK_LMETA:                return BX_KEY_LMETA;
+    case SDLK_LMETA:                return BX_KEY_WIN_L;
     case SDLK_LSUPER:               return BX_KEY_WIN_L;
     case SDLK_RSUPER:               return BX_KEY_WIN_R;
 //  case SDLK_MODE:                 return BX_KEY_MODE;
