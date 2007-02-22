33e6efa0e04387f9f404f5d1e9a05f9496716025
sshwarts <sshwarts>
2007-02-22 17:43:29 +0000

    Remove incorrect assertion

--- disasm/dis_groups.cc
+++ disasm/dis_groups.cc
@@ -563,7 +563,6 @@ void disassembler::Jw(const x86_insn *insn)
 {
   // Jw supported in 16-bit mode only
   assert(! insn->is_64);
-  assert(! insn->is_32);
 
   Bit16u imm16 = (Bit16s) fetch_word();
   dis_sprintf(".+0x%04x", (unsigned) imm16);
