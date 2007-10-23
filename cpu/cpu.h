bdf24a672003df98e1891392d2745f96ef313827
sshwarts <sshwarts>
2007-10-23 21:51:42 +0000

    Remove dump_cpu debugger function, CPI method and all related structures. Extended 'info' command in debugger to have all functionality of dump_cpu if needed. Also param tree print always could be used !

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -2773,7 +2773,6 @@ public: // for now...
   BX_SMF void     dbg_take_irq(void);
   BX_SMF void     dbg_force_interrupt(unsigned vector);
   BX_SMF void     dbg_take_dma(void);
-  BX_SMF bx_bool  dbg_get_cpu(bx_dbg_cpu_t *cpu);
   BX_SMF bx_bool  dbg_set_reg(unsigned reg, Bit32u val);
   BX_SMF Bit32u   dbg_get_reg(unsigned reg);
   BX_SMF bx_bool  dbg_get_sreg(bx_dbg_sreg_t *sreg, unsigned sreg_no);
