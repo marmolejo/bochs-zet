f5e3fbcc9fbacb123f5d0f5d7aca8a93b8c1b198
sshwarts <sshwarts>
2007-11-12 18:20:05 +0000

    Split JCC methods to 16 different methods per branch condition

--- cpu/resolve16.cc
+++ cpu/resolve16.cc
@@ -31,6 +31,12 @@
 #define LOG_THIS BX_CPU_THIS_PTR
 
   void BX_CPP_AttrRegparmN(1)
+BX_CPU_C::ResolveDummy(bxInstruction_c *i)
+{
+  return; // used when no memory access is needed
+}
+
+  void BX_CPP_AttrRegparmN(1)
 BX_CPU_C::Resolve16Mod0Rm0(bxInstruction_c *i)
 {
   RMAddr(i) = (Bit16u) (BX + SI);
