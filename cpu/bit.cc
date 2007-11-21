de26653476f68f0e674c423f1f32473421b57527
sshwarts <sshwarts>
2007-11-21 22:42:40 +0000

    a bit faster SETL/SETNL code

--- cpu/bit.cc
+++ cpu/bit.cc
@@ -192,12 +192,7 @@ void BX_CPU_C::SETNP_Eb(bxInstruction_c *i)
 
 void BX_CPU_C::SETL_Eb(bxInstruction_c *i)
 {
-  Bit8u result_8;
- 
-  if (getB_SF() != getB_OF())
-    result_8 = 1;
-  else
-    result_8 = 0;
+  Bit8u result_8 = (getB_SF() ^ getB_OF());
 
   /* now write result back to destination */
   if (i->modC0()) {
@@ -210,12 +205,7 @@ void BX_CPU_C::SETL_Eb(bxInstruction_c *i)
 
 void BX_CPU_C::SETNL_Eb(bxInstruction_c *i)
 {
-  Bit8u result_8;
- 
-  if (getB_SF() == getB_OF())
-    result_8 = 1;
-  else
-    result_8 = 0;
+  Bit8u result_8 = !(getB_SF() ^ getB_OF());
 
   /* now write result back to destination */
   if (i->modC0()) {
