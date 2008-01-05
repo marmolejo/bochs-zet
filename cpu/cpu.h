0a3bb33522c4404ef19214a4a22af3eed4a72db5
sshwarts <sshwarts>
2008-01-05 10:21:25 +0000

    Optimize short traces

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -3112,7 +3112,7 @@ public: // for now...
   BX_SMF unsigned fetchDecode64(Bit8u *fetchPtr, bxInstruction_c *i, unsigned remainingInPage);
 #endif
 #if BX_SUPPORT_TRACE_CACHE
-  BX_SMF bxICacheEntry_c* fetchInstructionTrace(bxInstruction_c *, bx_address);
+  BX_SMF bxInstruction_c* fetchInstructionTrace(bxInstruction_c *iStorage, unsigned *len, bx_address eipBiased);
   BX_SMF bx_bool mergeTraces(bxICacheEntry_c *trace, bxInstruction_c *, bx_phy_address pAddr);
   BX_SMF void instrumentTraces(void);
 #else
