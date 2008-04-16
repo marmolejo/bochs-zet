1d984504e3004171a48ad6ae1633ee9f91d05fa9
sshwarts <sshwarts>
2008-04-16 22:08:46 +0000

    Fixed canonical fault exceptions for call_far, interrupt, lldt and ltr instructions

--- cpu/exception.cc
+++ cpu/exception.cc
@@ -186,7 +186,7 @@ void BX_CPU_C::long_mode_int(Bit8u vector, bx_bool is_INT, bx_bool is_error_code
       // #SS(selector) when changing priviledge level
       BX_ERROR(("interrupt(long mode): canonical address failure %08x%08x",
          GET32H(RSP_for_cpl_x), GET32L(RSP_for_cpl_x)));
-      exception(BX_GP_EXCEPTION, old_SS & 0xfffc, 0);
+      exception(BX_SS_EXCEPTION, old_SS & 0xfffc, 0);
     }
 
     // push old stack long pointer onto new stack
