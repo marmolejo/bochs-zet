978e358fb2fd93bdd6695ea406a637f49e68a0fb
sshwarts <sshwarts>
2008-04-19 13:21:21 +0000

    Added debugger memory trace functionality. Enable by 'trace-mem on' command

--- cpu/init.cc
+++ cpu/init.cc
@@ -949,6 +949,7 @@ void BX_CPU_C::reset(unsigned source)
   BX_CPU_THIS_PTR stop_reason = STOP_NO_REASON;
   BX_CPU_THIS_PTR magic_break = 0;
   BX_CPU_THIS_PTR trace_reg = 0;
+  BX_CPU_THIS_PTR trace_mem = 0;
 #endif
 
   BX_CPU_THIS_PTR trace = 0;
