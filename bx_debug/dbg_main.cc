c80bac52d985ecd65b680d461cd3918684f62375
sshwarts <sshwarts>
2008-07-11 17:31:30 +0000

    Re-apply SF patch "Set new default format and unit only if both are supported"

--- bx_debug/dbg_main.cc
+++ bx_debug/dbg_main.cc
@@ -2303,12 +2303,6 @@ void bx_dbg_examine_command(char *command, char *format, bx_bool format_passed,
           break;
       }
     }
-
-    if (format_passed) {
-      // store current options as default
-      bx_debugger.default_display_format = display_format;
-      bx_debugger.default_unit_size      = unit_size;
-    }
   }
 
   if ((display_format == 'i') || (display_format == 's')) {
@@ -2319,6 +2313,12 @@ void bx_dbg_examine_command(char *command, char *format, bx_bool format_passed,
   if (unit_size == 'g') {
     dbg_printf("error: dbg_examine: 'g' (8-byte) unit size not supported.\n");
     return;
+  }
+
+  if (format_passed) {
+    // store current options as default
+    bx_debugger.default_display_format = display_format;
+    bx_debugger.default_unit_size      = unit_size;
   }
 
   data_size = 0;
