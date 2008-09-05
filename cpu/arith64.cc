a8723da7583aa98ce59c8f615dd5808d89a7debc
sshwarts <sshwarts>
2008-09-05 21:43:12 +0000

    -Fixed alignment chek to be on laddr instead of eaddr for CMPXCHG16B

--- cpu/arith64.cc
+++ cpu/arith64.cc
@@ -563,7 +563,9 @@ void BX_CPP_AttrRegparmN(1) BX_CPU_C::CMPXCHG16B(bxInstruction_c *i)
 
   bx_address eaddr = BX_CPU_CALL_METHODR(i->ResolveModrm, (i));
 
-  if (eaddr & 0xf) {
+  bx_address laddr = get_laddr64(i->seg(), eaddr);
+
+  if (laddr & 0xf) {
     BX_ERROR(("CMPXCHG16B: not aligned memory location (#GP)"));
     exception(BX_GP_EXCEPTION, 0, 0);
   }
