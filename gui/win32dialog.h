e0f00cd0c113d33e035e2506a48c1633a5d48828
sshwarts <sshwarts>
2008-10-21 13:45:03 +0000

    - Added graphical Bochs debugger under Win32 host !   - Thanks for Chourdakis Michael and Bruce Ewing.

--- gui/win32dialog.h
+++ gui/win32dialog.h
@@ -4,11 +4,29 @@
 
 #include "config.h"
 
+#ifndef _WIN32_IE
+#define _WIN32_IE 0x0400	// Force a minimum "OS level" for commctrl.h
+#endif
+#if _WIN32_IE < 0x0400
+#undef _WIN32_IE
+#define _WIN32_IE 0x0400
+#endif
+
+extern "C" {
+#include <assert.h>
+#include <stdio.h>
+#include <windows.h>
+#include <commctrl.h>
+#include <shlobj.h>
+#include <ctype.h>
+}
+
 #if BX_USE_TEXTCONFIG && defined(WIN32)
 
 int RuntimeOptionsDialog();
 #if BX_DEBUGGER
 void InitDebugDialog(HWND mainwnd);
+void RefreshDebugDialog();
 #endif
 void win32_init_notify_callback();
 
