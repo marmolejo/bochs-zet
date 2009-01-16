651f2d2796ab15211a30a8e41207781a86541c38
sshwarts <sshwarts>
2009-01-16 18:18:57 +0000

    - updated FSF address

--- pc_system.h
+++ pc_system.h
@@ -22,7 +22,9 @@
 //
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+/////////////////////////////////////////////////////////////////////////
 
 #ifndef BX_PCSYS_H
 #define BX_PCSYS_H
