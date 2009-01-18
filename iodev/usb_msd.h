5bad634d922a2a38562eed8ca8beb5ef9ff17f87
vruppert <vruppert>
2009-01-18 13:11:27 +0000

    - moved common USB device code and USB definitons to separate files - updated dependencies - updated FSF address

--- iodev/usb_msd.h
+++ iodev/usb_msd.h
@@ -2,7 +2,7 @@
 // $Id$
 /////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2007  Volker Ruppert
+//  Copyright (C) 2009  Volker Ruppert
 //
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
@@ -16,7 +16,7 @@
 //
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 // USB mass storage device support ported from the Qemu project
 
