19205ee4da9a3c6844bf82ba422c65fa0f824beb
vruppert <vruppert>
2009-03-29 00:21:10 +0000

    - fixed checks for return value of AskFilename() - added hotkeys for the start menu dialog

--- gui/win32dialog.cc
+++ gui/win32dialog.cc
@@ -542,7 +542,7 @@ static BOOL CALLBACK StartMenuDlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM
           rcfile = new bx_param_filename_c(NULL, "rcfile", "Load Bochs Config File",
                                            "", "bochsrc.bxrc", BX_PATHNAME_LEN);
           rcfile->set_extension("bxrc");
-          if (AskFilename(hDlg, rcfile, NULL)) {
+          if (AskFilename(hDlg, rcfile, NULL) > 0) {
             SIM->reset_all_param();
             SIM->read_rc(rcfile->getptr());
           }
@@ -553,7 +553,7 @@ static BOOL CALLBACK StartMenuDlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM
                                            "", "bochsrc.bxrc", BX_PATHNAME_LEN);
           rcfile->set_extension("bxrc");
           rcfile->set_options(rcfile->SAVE_FILE_DIALOG);
-          if (AskFilename(hDlg, rcfile, NULL)) {
+          if (AskFilename(hDlg, rcfile, NULL) > 0) {
             SIM->write_rc(rcfile->getptr(), 1);
           }
           delete rcfile;
