b7ff0e9baf6e28f768ca110ef978d583dbb7a84c
sshwarts <sshwarts>
2009-08-04 16:19:15 +0000

    bugfix

--- cpu/soft_int.cc
+++ cpu/soft_int.cc
@@ -138,7 +138,7 @@ void BX_CPP_AttrRegparmN(1) BX_CPU_C::INT_Ib(bxInstruction_c *i)
       }
 
       Bit32u io_base = system_read_word(tr_base + 102), offset = io_base - 32 + (vector >> 3);
-      if (offset >= BX_CPU_THIS_PTR tr.cache.u.segment.limit_scaled) {
+      if (offset > BX_CPU_THIS_PTR tr.cache.u.segment.limit_scaled) {
         BX_ERROR(("INT_Ib(): failed to fetch VME redirection bitmap"));
         exception(BX_GP_EXCEPTION, 0, 0);
       }
