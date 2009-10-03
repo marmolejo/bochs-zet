a259d0d7c9513e80d85c33fcf2d9e87cc3567909
sshwarts <sshwarts>
2009-10-03 11:39:29 +0000

    compilation fix

--- cpu/paging.cc
+++ cpu/paging.cc
@@ -1053,9 +1053,11 @@ bx_phy_address BX_CPU_C::translate_linear_PAE(bx_address laddr, bx_address &lpf_
   unsigned pl = (curr_pl == 3);
   int fault;
 
+#if BX_SUPPORT_X86_64
   if (long_mode()) {
     return translate_linear_long_mode(laddr, lpf_mask, combined_access, curr_pl, rw);
   }
+#endif
 
   combined_access = 0x06;
 
