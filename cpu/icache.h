7e9800df5a9f944c0c82ce7135dcf7297c50f4de
sshwarts <sshwarts>
2009-10-14 20:45:29 +0000

    copyright fixes + small optimization

--- cpu/icache.h
+++ cpu/icache.h
@@ -2,7 +2,7 @@
 // $Id$
 /////////////////////////////////////////////////////////////////////////
 //
-//   Copyright (c) 2007 Stanislav Shwartsman
+//   Copyright (c) 2007-2009 Stanislav Shwartsman
 //          Written by Stanislav Shwartsman [sshwarts at sourceforge net]
 //
 //  This library is free software; you can redistribute it and/or
