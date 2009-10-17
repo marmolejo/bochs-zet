e8e9e5831f08e6acb295d94ad87967771bf4a5ef
sshwarts <sshwarts>
2009-10-17 18:17:28 +0000

    read 64-bit params for 'memory'

--- main.cc
+++ main.cc
@@ -1053,10 +1053,10 @@ void bx_init_hardware()
 
   // set up memory and CPU objects
   bx_param_num_c *bxp_memsize = SIM->get_param_num(BXPN_MEM_SIZE);
-  Bit64u memSize = bxp_memsize->get() * 1024*1024;
+  Bit64u memSize = bxp_memsize->get64() * BX_CONST64(1024*1024);
 
   bx_param_num_c *bxp_host_memsize = SIM->get_param_num(BXPN_HOST_MEM_SIZE);
-  Bit64u hostMemSize = bxp_host_memsize->get() * 1024*1024;
+  Bit64u hostMemSize = bxp_host_memsize->get64() * BX_CONST64(1024*1024);
 
   // do not allocate more host memory than needed for emulation of guest RAM 
   if (memSize < hostMemSize) hostMemSize = memSize;
