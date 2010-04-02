b10295d19b0d8cf3149e9e4c8f613058d72ef881
sshwarts <sshwarts>
2010-04-02 18:24:27 +0000

    bugfix

--- cpu/call_far.cc
+++ cpu/call_far.cc
@@ -61,7 +61,7 @@ BX_CPU_C::call_protected(bxInstruction_c *i, Bit16u cs_raw, bx_address disp)
     check_cs(&cs_descriptor, cs_raw, BX_SELECTOR_RPL(cs_raw), CPL);
 
 #if BX_SUPPORT_X86_64
-    if (cs_descriptor.u.segment.l) {
+    if (long_mode() && cs_descriptor.u.segment.l) {
       Bit64u temp_rsp = RSP; 
       // moving to long mode, push return address onto 64-bit stack
       if (i->os64L()) {
